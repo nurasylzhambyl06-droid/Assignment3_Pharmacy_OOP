80ad2314774c148342a7dfe29997722ed77ef3fd
nurasylzhambyl06-droid <nurasylzhambyl06@gmail.com>
2026-01-24 19:46:24 +0500

    Demonstrate polymorphism and CRUD usage

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,30 @@
 package org.example;
 
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-public class Main {
-    static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
+import model.BaseEntity;
+import service.MedicineService;
+import service.PatientService;
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+public class Main {
+
+    public static void main(String[] args) {
+
+        PatientService patientService = new PatientService();
+        MedicineService medicineService = new MedicineService();
+
+        try {
+            for (BaseEntity p : patientService.getAllPatients()) {
+                System.out.println(p.getInfo());
+            }
+
+            for (BaseEntity m : medicineService.getAllMedicines()) {
+                System.out.println(m.getInfo());
+            }
+
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
         }
+        BaseEntity entity = new Medicine(99, "TestMed", 100.0);
+        System.out.println(entity.getInfo());
     }
 }
+
