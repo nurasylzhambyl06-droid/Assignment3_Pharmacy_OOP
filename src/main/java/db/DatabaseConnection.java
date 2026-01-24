324609a35a0b350c7eb6937b581377e2a3c676b7
nurasylzhambyl06-droid <nurasylzhambyl06@gmail.com>
2026-01-24 18:34:02 +0500

    Configure JDBC connection

--- src/main/java/db/DatabaseConnection.java
+++ src/main/java/db/DatabaseConnection.java
@@ -1,4 +1,17 @@
 package db;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class DatabaseConnection {
+
+    public static Connection getConnection() throws SQLException {
+        return DriverManager.getConnection(
+                "jdbc:postgresql://localhost:5433/pharmacy",
+                "postgres",
+                "nur@syl2439"
+        );
+    }
 }
+
