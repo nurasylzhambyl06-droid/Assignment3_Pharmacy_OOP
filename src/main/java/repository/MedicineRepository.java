e290f254cffb587854e2524f517617bc23401ec2
nurasylzhambyl06-droid <nurasylzhambyl06@gmail.com>
2026-01-24 18:58:00 +0500

    implement CRUD operations with PreparedStatement

--- src/main/java/repository/MedicineRepository.java
+++ src/main/java/repository/MedicineRepository.java
@@ -1,4 +1,28 @@
 package repository;
 
+import db.DatabaseConnection;
+import model.Medicine;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class MedicineRepository {
+
+    public List<Medicine> getAll() throws SQLException {
+        List<Medicine> list = new ArrayList<>();
+        Connection con = DatabaseConnection.getConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM medicines");
+
+        while (rs.next()) {
+            list.add(new Medicine(
+                    rs.getInt("medicine_id"),
+                    rs.getString("medicine_name"),
+                    rs.getDouble("price")
+            ));
+        }
+        return list;
+    }
 }
+
--- src/main/java/repository/PatientRepository.java
+++ src/main/java/repository/PatientRepository.java
@@ -1,4 +1,48 @@
 package repository;
 
+import db.DatabaseConnection;
+import model.Patient;
+
+import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
+
 public class PatientRepository {
+
+    public List<Patient> getAll() throws SQLException {
+        List<Patient> list = new ArrayList<>();
+        Connection con = DatabaseConnection.getConnection();
+        Statement st = con.createStatement();
+        ResultSet rs = st.executeQuery("SELECT * FROM patients");
+
+        while (rs.next()) {
+            list.add(new Patient(
+                    rs.getInt("patient_id"),
+                    rs.getString("full_name"),
+                    rs.getString("phone")
+            ));
+        }
+        return list;
+    }
+    public Patient getById(int id) throws SQLException {
+        String sql = "SELECT * FROM patients WHERE patient_id = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ResultSet rs = ps.executeQuery();
+        if (rs.next()) {
+            return new Patient(
+                    rs.getInt("patient_id"),
+                    rs.getString("full_name"),
+                    rs.getString("phone")
+            );
+        }
+        return null;
+    }
+    public void delete(int id) throws SQLException {
+        String sql = "DELETE FROM patients WHERE patient_id = ?";
+        PreparedStatement ps = con.prepareStatement(sql);
+        ps.setInt(1, id);
+        ps.executeUpdate();
+    }
 }
+
