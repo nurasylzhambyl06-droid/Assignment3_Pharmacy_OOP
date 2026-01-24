0b2a0155faf2f74d46d5ef9da45f55a3d7a3798e
nurasylzhambyl06-droid <nurasylzhambyl06@gmail.com>
2026-01-24 19:39:09 +0500

    add validation and business rules

--- src/main/java/service/MedicineService.java
+++ src/main/java/service/MedicineService.java
@@ -1,4 +1,22 @@
 package service;
 
+import exceptions.DatabaseOperationException;
+import model.Medicine;
+import repository.MedicineRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class MedicineService {
+
+    private MedicineRepository repo = new MedicineRepository();
+
+    public List<Medicine> getAllMedicines() throws DatabaseOperationException {
+        try {
+            return repo.getAll();
+        } catch (SQLException e) {
+            throw new DatabaseOperationException("Cannot load medicines");
+        }
+    }
 }
+
--- src/main/java/service/PatientService.java
+++ src/main/java/service/PatientService.java
@@ -1,4 +1,22 @@
 package service;
 
+import exceptions.DatabaseOperationException;
+import model.Patient;
+import repository.PatientRepository;
+
+import java.sql.SQLException;
+import java.util.List;
+
 public class PatientService {
+
+    private PatientRepository repo = new PatientRepository();
+
+    public List<Patient> getAllPatients() throws DatabaseOperationException {
+        try {
+            return repo.getAll();
+        } catch (SQLException e) {
+            throw new DatabaseOperationException("Cannot load patients");
+        }
+    }
 }
+
