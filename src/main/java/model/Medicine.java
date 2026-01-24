4ac0196f000bdc60c3f7693b503a73cfc5f1bd64
nurasylzhambyl06-droid <nurasylzhambyl06@gmail.com>
2026-01-24 17:35:30 +0500

    add BaseEntity and core entities

--- src/main/java/model/Medicine.java
+++ src/main/java/model/Medicine.java
@@ -1,4 +1,27 @@
 package model;
 
-public class Medicine {
+public class Medicine extends BaseEntity {
+
+    private double price;
+
+    public Medicine(int id, String name, double price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    @Override
+    public String getInfo() {
+        return "Medicine " + name + ", price: " + price;
+    }
+
+    @Override
+    public boolean isValid() {
+        return price > 0;
+    }
 }
+
